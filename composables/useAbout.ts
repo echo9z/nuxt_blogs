82911fcb321a7c824f337e24b5a58cadc4f8eb99
echo9z <1131223522@qq.com>
2023-02-26 01:50:38 +0800

    perf: 修改about关于内容

--- composables/useAbout.ts
+++ composables/useAbout.ts
@@ -12,11 +12,11 @@ export function useAbout() {
   ### 前端 博客页（不分前后台）：
   - TS + Nuxt3（存在坑）
   - CSS院子库 tailwindcss
-  - 状态集中管理工具 Pinia
+  - 状态集中管理工具 [Pinia](https://pinia.vuejs.org/zh/)
   - 前端路由 Vue-Router
   - markdown格式渲染工具 [md-editor-v3](https://github.com/imzbf/md-editor-v3)
-  - 表单校验 [vee-validate]()
-  - 组件数据懒加载 vueuse
+  - 表单校验 [vee-validate](https://vee-validate.logaretm.com/v4/)
+  - 组件数据懒加载 [vueuse](https://vueuse.org/)
   
   ### 后端：
   - 后端使用Nest.js框架，包括：
