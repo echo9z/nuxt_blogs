0c2541c3113e6ac3bf1f178fe0f9153ef7c25afe
echo9z <1131223522@qq.com>
2023-04-01 06:08:02 +0800

    ci:测试ci/cd

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,4 @@
 // https://nuxt.com/docs/getting-started/configuration
 export default defineAppConfig({
   title: 'Hello Nuxt',
-  theme: {
-    dark: true,
-    colors: {
-      primary: '#ff0000'
-    }
-  }
 })
